e929f450d99fe4b1842c7f9cb26f678a554fbce4
Anino1996 <anino1996.ao@gmail.com>
2021-12-07 15:56:52 -0800

    Adding project files

---
+++ src/main/java/com/capstone/AninPringleOfori/AninPringleOforiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.AninPringleOfori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AninPringleOforiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AninPringleOforiApplication.class, args);
+	}
+
+}
