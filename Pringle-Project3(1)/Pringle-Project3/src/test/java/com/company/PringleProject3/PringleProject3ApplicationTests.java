2ad324c7a7fd93a241465d3641c82cd5412ec8d1
Pringle <89086150+PRQFESSQRX@users.noreply.github.com>
2021-12-14 18:50:39 -0500

    Add files via upload

---
+++ Pringle-Project3(1)/Pringle-Project3/src/test/java/com/company/PringleProject3/PringleProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.PringleProject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PringleProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Pringle-Project3(1)/Pringle-Project3/src/test/java/com/company/PringleProject3/dao/ConsoleDaoTest.java
@@ -0,0 +1,4 @@
+package com.company.PringleProject3.dao;
+
+public class ConsoleDaoTest {
+}
