94f1491c352446ad15e6914ac3cd415d023056e6
Anino1996 <anino1996.ao@gmail.com>
2021-12-15 21:46:27 -0800

    Enabling oauth - Controller tests not working with oauth

--- src/main/java/com/capstone/AninPringleOfori/AninPringleOforiApplication.java
+++ src/main/java/com/capstone/AninPringleOfori/AninPringleOforiApplication.java
@@ -2,9 +2,9 @@ package com.capstone.AninPringleOfori;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-//@EnableResourceServer
+@EnableResourceServer
 @SpringBootApplication
 public class AninPringleOforiApplication {
 	public static void main(String[] args) {
